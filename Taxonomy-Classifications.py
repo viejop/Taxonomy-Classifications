2d639384a3c3a04dbed4ee9b874597438c59eb6a
viejop <158598912+viejop@users.noreply.github.com>
2025-03-10 02:18:45 -0400

    Create Taxonomy-Classifications.py

---
+++ Taxonomy-Classifications.py
@@ -0,0 +1,21 @@
+                                                                    
+print (" ████████                           ████████                        
+         ███  ██████                        ███ ██████                      
+         ███     ███   ███████   █████████  ██      ███   ███████  ███  ███ 
+         ███      ██  ███   ███ ███    ███  ██      ███ ███    ███  ██████  
+         ███      ██ ███    ███ ███    ███  ██      ███ ██████████   ████   
+         ███    ████  ███   ███ ███   ████  ██     ███  ███         ██████  
+         ██████████   ████████   █████████  █████████    █████████ ███  ███ 
+                                      ███                                   
+                                 ███████                                    
+                                                                    ")
+
+Kingdom: Animalia
+Phylum: Chordata
+Class: Mammalia
+Order: Carnivora
+Family: Canidae
+Genus: Canis
+Species: Canis lupus
+Subspecies: Canis lupus familiaris
+Breed: Beagle

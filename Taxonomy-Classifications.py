9476dd01b3660211988774aac07834c4d0da239c
viejop <158598912+viejop@users.noreply.github.com>
2025-03-11 13:52:40 -0400

    Update Taxonomy-Classifications.py

--- Taxonomy-Classifications.py
+++ Taxonomy-Classifications.py
@@ -19,3 +19,81 @@ Genus: Canis
 Species: Canis lupus
 Subspecies: Canis lupus familiaris
 Breed: Beagle
+
+
+Labrador Retriever
+
+Kingdom: Animalia
+Phylum: Chordata
+Class: Mammalia
+Order: Carnivora
+Family: Canidae
+Genus: Canis
+Species: Canis lupus familiaris
+Breed: Labrador Retriever
+
+German Shepherd
+
+Kingdom: Animalia
+Phylum: Chordata
+Class: Mammalia
+Order: Carnivora
+Family: Canidae
+Genus: Canis
+Species: Canis lupus familiaris
+Breed: German Shepherd
+
+Bulldog
+
+Kingdom: Animalia
+Phylum: Chordata
+Class: Mammalia
+Order: Carnivora
+Family: Canidae
+Genus: Canis
+Species: Canis lupus familiaris
+Breed: Bulldog
+
+Poodle
+
+Kingdom: Animalia
+Phylum: Chordata
+Class: Mammalia
+Order: Carnivora
+Family: Canidae
+Genus: Canis
+Species: Canis lupus familiaris
+Breed: Poodle
+
+Dachshund
+
+Kingdom: Animalia
+Phylum: Chordata
+Class: Mammalia
+Order: Carnivora
+Family: Canidae
+Genus: Canis
+Species: Canis lupus familiaris
+Breed: Dachshund
+
+Chihuahua
+
+Kingdom: Animalia
+Phylum: Chordata
+Class: Mammalia
+Order: Carnivora
+Family: Canidae
+Genus: Canis
+Species: Canis lupus familiaris
+Breed: Chihuahua
+
+Golden Retriever
+
+Kingdom: Animalia
+Phylum: Chordata
+Class: Mammalia
+Order: Carnivora
+Family: Canidae
+Genus: Canis
+Species: Canis lupus familiaris
+Breed: Golden Retriever
